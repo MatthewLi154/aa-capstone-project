b2cba883428f9d8a0aad506f50b5dd3d6b5099a9
MatthewLi154 <matthewli@matts-laptop.localdomain>
2022-11-22 09:52:00 -0800

    laptop navbar commit

--- react-app/src/components/Main/index.js
+++ react-app/src/components/Main/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import "./main.css"
 
 const Main = () => {
     return (
         <>
-        <h1>You are on the main page</h1>
-        <img src="../../images/n-logo.png"></img>
+            <h1>You are on the main page</h1>
+            <img src="https://i.ibb.co/c8mk2g0/n-logo-2.png" alt="n-logo-2" border="0"/>
         </>
     )
 }
--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -5,32 +5,103 @@ import LogoutButton from '../auth/LogoutButton';
 import "./NavBar.css"
 
 const NavBar = () => {
+
+  const loggedOut = (<nav>
+    <div className="navbar-main-container">
+      <div className="nav-button-container">
+        <NavLink to="/" exact={true} style={{ textDecoration: 'none' }}><span>N</span></NavLink>
+      </div>
+      <div className="nav-button-container">
+        <NavLink to='/' exact={true} style={{ textDecoration: 'none' }} activeClassName='active'>
+          Home
+        </NavLink>
+      </div>
+      <div className="nav-button-container">
+        <NavLink to='/' exact={true} style={{ textDecoration: 'none' }} activeClassName='active'>
+          Today
+        </NavLink>
+      </div>
+      <div className="nav-button-container">
+        <NavLink to='/' exact={true} style={{ textDecoration: 'none' }} activeClassName='active'>
+          Create
+        </NavLink>
+      </div>
+      <i class="fa-solid fa-magnifying-glass"></i>
+      <div className="nav-button-container search-container">
+        <input placeholder='Search'></input>
+      </div>
+      <div className="nav-button-container">
+        <NavLink to='/login' style={{ textDecoration: 'none' }} exact={true} activeClassName='active'>
+          Login
+        </NavLink>
+      </div>
+      <div className="nav-button-container">
+        <NavLink to='/sign-up' style={{ textDecoration: 'none' }} exact={true} activeClassName='active'>
+          Sign Up
+        </NavLink>
+      </div>
+      <div className="nav-button-container">
+        <NavLink to='/users' style={{ textDecoration: 'none' }} exact={true} activeClassName='active'>
+          Users
+        </NavLink>
+      </div>
+      <div className="nav-button-container">
+        <LogoutButton />
+      </div>
+    </div>
+  </nav>)
+
   return (
     <nav>
       <div className="navbar-main-container">
-        <div>
-          <NavLink to='/' exact={true} activeClassName='active'>
+        <div className="nav-button-container">
+          <NavLink to="/" exact={true} style={{ textDecoration: 'none' }}><span>N</span></NavLink>
+        </div>
+        <div className="nav-button-container">
+          <NavLink to='/' exact={true} style={{ textDecoration: 'none' }} activeClassName='active'>
             Home
           </NavLink>
         </div>
+        <div className="nav-button-container">
+          <NavLink to='/' exact={true} style={{ textDecoration: 'none' }} activeClassName='active'>
+            Today
+          </NavLink>
+        </div>
+        <div className="nav-button-container">
+          <NavLink to='/' exact={true} style={{ textDecoration: 'none' }} activeClassName='active'>
+            Create
+          </NavLink>
+        </div>
+        <i class="fa-solid fa-magnifying-glass"></i>
+        <div className="nav-button-container search-container">
+          <input placeholder='Search'></input>
+        </div>
         <div>
-          <NavLink to='/login' exact={true} activeClassName='active'>
+          <div className="navbar-profile-picture">
+            <img src="https://i.pinimg.com/564x/08/13/5c/08135cd812b33ad4788956ac2980898f.jpg"></img>
+          </div>
+        </div>
+        <div>
+          <i class="fa-solid fa-angle-down"></i>
+        </div>
+        {/* <div className="nav-button-container">
+          <NavLink to='/login' style={{ textDecoration: 'none' }} exact={true} activeClassName='active'>
             Login
           </NavLink>
         </div>
-        <div>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
+        <div className="nav-button-container">
+          <NavLink to='/sign-up' style={{ textDecoration: 'none' }} exact={true} activeClassName='active'>
             Sign Up
           </NavLink>
         </div>
-        <div>
-          <NavLink to='/users' exact={true} activeClassName='active'>
+        <div className="nav-button-container">
+          <NavLink to='/users' style={{ textDecoration: 'none' }} exact={true} activeClassName='active'>
             Users
           </NavLink>
         </div>
-        <div>
+        <div className="nav-button-container">
           <LogoutButton />
-        </div>
+        </div> */}
       </div>
     </nav>
   );
