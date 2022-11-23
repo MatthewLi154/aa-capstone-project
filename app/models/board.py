72a5cb5a6a88f84538b03073bef4c1b70aed27d7
MatthewLi154 <matthewli154@MATTS_IPHONE.localdomain>
2022-11-23 14:48:06 -0800

    migrations models

--- app/models/board.py
+++ app/models/board.py
@@ -1,9 +1,8 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
-board_pin = db.Table('board_pin',
-    db.Model.metadata,
-    db.Column('pins_id', db.Integer, db.ForeignKey(add_prefix_for_prod('pins.id'))),
-    db.Column('boards_id', db.Integer, db.ForeignKey(add_prefix_for_prod('boards.id')))
+board_pins = db.Table('board_pins',
+    db.Column('pins_id', db.Integer, db.ForeignKey(add_prefix_for_prod('pins.id')), primary_key=True),
+    db.Column('boards_id', db.Integer, db.ForeignKey(add_prefix_for_prod('boards.id')), primary_key=True)
 )
 
 class Board(db.Model):
@@ -17,4 +16,4 @@ class Board(db.Model):
     profile_id = db.Column(db.Integer, nullable=False)
     createdAt = db.Column(db.String(55), nullable=False)
 
-    pins = db.relationship('Pin', secondary=board_pin, back_populates="boards")
+    pins = db.relationship('Pin', secondary=board_pins, back_populates="boards")
