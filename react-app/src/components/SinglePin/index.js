6458415a52550a4e6fa591e8dfe0db0c563c55fc
MatthewLi154 <matthewli154@MATTS_IPHONE.localdomain>
2022-11-30 15:05:21 -0800

    userBoards conditional rendering on singlepin

--- react-app/src/components/SinglePin/index.js
+++ react-app/src/components/SinglePin/index.js
@@ -126,7 +126,7 @@ const SinglePin = () => {
 
   return (
     <>
-      {currentProfile && (
+      {userBoards && (
         <div className="main-single-pin-page">
           <div className="main-single-pin-container">
             <div className="single-pin-left-container">
